b9b99ce32e782f2fd5dc4791cb1dec02fe925adf
Lane-eb <zhangjian@easternbay.cn>
2021-02-13 20:35:06 +0800

    Finish helloworld android official sample

--- app/src/main/java/com/example/helloworld1/MainActivity.kt
+++ app/src/main/java/com/example/helloworld1/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.helloworld1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+//import android.view.View
 import android.widget.RadioButton
+
+const val EXTRA_MESSAGE = "com.example.helloworld1.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,22 +16,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onRadioButtonClicked(view: View) {
-        if (view is RadioButton) {
-            // Is the button now checked?
-            val checked = view.isChecked
-
-            // Check which radio button was clicked
-            when (view.getId()) {
-                R.id.radio_pirates ->
-                    if (checked) {
-                        // Pirates are the best
-                    }
-                R.id.radio_ninjas ->
-                    if (checked) {
-                        // Ninjas rule
-                    }
-            }
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
         }
+        startActivity(intent)
     }
+
+
+//    fun onRadioButtonClicked(view: View) {
+//        if (view is RadioButton) {
+//            // Is the button now checked?
+//            val checked = view.isChecked
+//
+//            // Check which radio button was clicked
+//            when (view.getId()) {
+//                R.id.radio_pirates ->
+//                    if (checked) {
+//                        // Pirates are the best
+//                    }
+//                R.id.radio_ninjas ->
+//                    if (checked) {
+//                        // Ninjas rule
+//                    }
+//            }
+//        }
+//    }
 }
